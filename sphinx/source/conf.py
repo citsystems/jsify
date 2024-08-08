923e9cb5ba748d99ce01b55854612ffc559ea34c
rajez <zbigniew.r@citsystems.pl>
2024-08-08 17:41:32 +0200

    Docs update with sitemap.xml

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -24,7 +24,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints']
+    'sphinx_autodoc_typehints',
+    'sphinx_sitemap',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -36,3 +38,4 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_baseurl = 'https://citsystems.github.io/jsify/'
